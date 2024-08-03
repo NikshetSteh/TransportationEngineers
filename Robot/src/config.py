9d2faa37d6d37c5efeebdf8c435ea7ca1702af24
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2024-08-03 20:27:02 +0500

    Small fix merge

--- Robot/src/config.py
+++ Robot/src/config.py
@@ -9,11 +9,8 @@ class Config(BaseSettings):
 
     BASE_API_URL: str = "http://localhost:8080/base_api/v1"
     STORE_API_URL: str = "http://localhost:8080/store_api/v1"
-<<<<<<< HEAD
-=======
 
     RESOURCE_URL: str = "http://localhost:8080"
->>>>>>> dev
 
     ROBOT_MODEL_ID: str = "TestRobotID1"
     ROBOT_MODEL_NAME: str = "TestRobot:0.1.0:001"
