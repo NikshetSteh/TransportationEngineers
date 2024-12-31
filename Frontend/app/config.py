084848409b8603b282668944d2e9f07b731b95b1
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2024-12-31 17:08:17 +0500

    Create login system with keycloak

---
+++ Frontend/app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Config(BaseSettings):
+    client_id: str = "ai_site"
+    client_secret: str = "7mbHVQGTLUKSNXrNYowg26SMCKv1YeLe"
+
+    auth_redirect_uri: str = "http://localhost:8090/realms/ai_site/protocol/openid-connect/auth"
+    auth_token_uri: str = "http://localhost:8090/realms/ai_site/protocol/openid-connect/token"
+
+
+@lru_cache
+def get_config() -> Config:
+    return Config()
--- Frontend/app/router.py
+++ Frontend/app/router.py
@@ -1,6 +1,12 @@
+import aiohttp
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from config import get_config
+import urllib.parse as urllib
+
+config = get_config()
 
 router = APIRouter()
 
@@ -15,21 +21,57 @@ async def root(request: Request) -> HTMLResponse:
 
 
 @router.get("/login")
-async def login(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(
-        request=request, name="login.html"
-    )
+async def auth(request: Request, code: str = None) -> RedirectResponse:
+    if code is None:
+        if "refresh_token" in request.cookies:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                        config.auth_token_uri,
+                        data={
+                            "client_id": config.client_id,
+                            "client_secret": config.client_secret,
+                            "grant_type": "refresh_token",
+                            "refresh_token": request.cookies["refresh_token"],
+                        },
+                ) as response:
+                    if response.status == 200:
+                        data = await response.json()
 
+                        response = RedirectResponse(
+                            url="http://localhost:8000/"
+                        )
 
-@router.get("/face_login")
-async def face_login(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(
-        request=request, name="face_login.html"
-    )
+                        response.set_cookie("access_token", data["access_token"])
+                        response.set_cookie("refresh_token", data["refresh_token"])
 
+                        return response
 
-@router.get("/qr_login")
-async def qr_login(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(
-        request=request, name="qr_login.html"
-    )
+        return RedirectResponse(
+            url=config.auth_redirect_uri + "?" + urllib.urlencode({
+                "response_type": "code",
+                "client_id": config.client_id,
+                "redirect_uri": request.url_for("auth"),
+            })
+        )
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                config.auth_token_uri,
+                data={
+                    "client_id": config.client_id,
+                    "client_secret": config.client_secret,
+                    "grant_type": "authorization_code",
+                    "code": code,
+                    "redirect_uri": request.url_for("auth"),
+                },
+        ) as response:
+            data = await response.json()
+
+            response = RedirectResponse(
+                url=request.url_for("root")
+            )
+
+            response.set_cookie("access_token", data["access_token"], expires=data["expires_in"])
+            response.set_cookie("refresh_token", data["refresh_token"], expires=data["refresh_expires_in"])
+
+            return response
