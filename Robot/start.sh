992fa7b28ea3b00d465e98af2e85966034242042
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2025-03-28 01:23:17 +0500

    Add robot startup file

---
+++ Robot/start.sh
@@ -0,0 +1,7 @@
+export BASE_API_URL="https://cufar.space/base_api/v1"
+export STORE_API_URL="https://cufar.space/store_api/v1"
+
+export RESOURCE_URL="https://cufar.space"
+
+cd src
+poetry run python main.py
